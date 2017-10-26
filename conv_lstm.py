d44fd1f09f4211c67307278d0c35b131fec8160a
Alienware <fahimi72@gmail.com>
2017-10-26 13:09:49 -0500

    progresss

--- conv_lstm.py
+++ conv_lstm.py
@@ -6,7 +6,6 @@ import torch
 
 
 
-
 def weights_init(m):
 	classname = m.__class__.__name__
 	if classname.find('Conv') != -1:
@@ -27,26 +26,26 @@ class Custom_CLSTM_cell(nn.Module):
 		super(Custom_CLSTM_cell, self).__init__()
 
 		self.encoding = nn.Sequential(
-			nn.Conv2d(3, 8, kernel_size=3, stride=(1,1,2,2)),
+			nn.Conv2d(3, 8, kernel_size=3, stride=(2),padding=(1,1)),
 			nn.ELU(inplace=True),
-			nn.Conv2d(8, 8, kernel_size=3, stride=(1,1,1,1)),
+			nn.Conv2d(8, 8, kernel_size=3, stride=(1),padding=1),
 			nn.ELU(inplace=True),
-			nn.Conv2d(8, 8, kernel_size=3, stride=(1,1,2,2)),
+			nn.Conv2d(8, 8, kernel_size=3, stride=(2),padding=(1,1)),
 			nn.ELU(inplace=True),
-			nn.Conv2d(8, 4, kernel_size=3, stride=(1,1,1,1)),
+			nn.Conv2d(8, 4, kernel_size=1, stride=(1)),
 			nn.ELU(inplace=True),
 		)
 
-		self.CLSTM = nn.Conv2d(8, 4* 4, kernel_size=3, stride=(1,1,1,1), 1)
+		self.CLSTM = nn.Conv2d(8, 4* 4, kernel_size=3, stride=(1), padding=1)
 
 		self.decoding = nn.Sequential(
-			nn.ConvTranspose2d(4, 8, kernel_size=1, stride=(1,1,1,1)),
+			nn.ConvTranspose2d(4, 8, kernel_size=1, stride=(1)),
 			nn.ELU(inplace=True),
-			nn.ConvTranspose2d(8, 8, kernel_size=3, stride=(1,1,2,2)),
+			nn.ConvTranspose2d(8, 8, kernel_size=2, stride=(2)),
 			nn.ELU(inplace=True),
-			nn.ConvTranspose2d(8, 8, kernel_size=3, stride=(1,1,1,1)),
-			nn.ELU(inplace=True),
-			nn.ConvTranspose2d(8, 3, kernel_size=3, stride=(1,1,2,2)),
+			nn.ConvTranspose2d(8, 3, kernel_size=2, stride=(2)),
+#			nn.ELU(inplace=True),
+#			nn.ConvTranspose2d(8, 3, kernel_size=2, stride=(2)),
 		)
 
 
@@ -54,8 +53,8 @@ class Custom_CLSTM_cell(nn.Module):
 	def forward(self, input, hidden_state):
 		hidden,c = hidden_state #hidden and c are images with several channels
 
-		encoding = self.encoding(input)
 
+		encoding = self.encoding(input)
 		combined = torch.cat((encoding, hidden), 1) #concatenate in the channels
 		B = self.CLSTM(combined)
 
@@ -68,9 +67,9 @@ class Custom_CLSTM_cell(nn.Module):
 		next_c= f*c+i*g
 		next_h=o*torch.tanh(next_c)
 
-		out = self.decoding()
 
-		return next_h, next_c
+		out = self.decoding(next_h)
+		return out, (next_h, next_c)
 
 	def init_hidden(self,batch_size):
 		return ( Variable(torch.zeros(batch_size, 4, 8, 8)).cuda(),
@@ -109,28 +108,33 @@ class CLSTM(nn.Module):
 			input is the tensor of shape seq_len,Batch,Chans,H,W
 
 		"""
-
 		current_input = input.transpose(0, 1)#now is seq_len,B,C,H,W
 		#current_input=input
 		next_hidden=[]#hidden states(h and c)
 		seq_len=current_input.size(0)
 
+#		for idlayer in xrange(self.num_layers):#loop for every layer
+#			hidden_c= hidden_state[idlayer]#hidden and c are images with several channels
+#			all_output = []
+#			output_inner = []
+#			for t in xrange(seq_len):#loop for every step
+#				output , hidden_c=self.cell_list[idlayer](current_input[t,...],hidden_c)
+#				output_inner.append(hidden_c[0])
+#
+#			next_hidden.append(hidden_c)
+#			current_input = torch.cat(output_inner, 0).view(current_input.size(0), *output_inner[0].size())#seq_len,B,chans,H,W
 
-		for idlayer in xrange(self.num_layers):#loop for every layer
+ #		return next_hidden, current_input
 
-			hidden_c=hidden_state[idlayer]#hidden and c are images with several channels
-			all_output = []
-			output_inner = []
-			for t in xrange(seq_len):#loop for every step
-				hidden_c=self.cell_list[idlayer](current_input[t,...],hidden_c)#cell_list is a list with different conv_lstms 1 for every layer
+		hidden_c = hidden_state[0]
+		out = [] 
+		for t in xrange(seq_len):
+			output, hidden_c = self.cell_list[0](current_input[t,...],hidden_c)
+			out.append(output)
+		next_hidden.append(hidden_c)	
 
-				output_inner.append(hidden_c[0])
-
-			next_hidden.append(hidden_c)
-			current_input = torch.cat(output_inner, 0).view(current_input.size(0), *output_inner[0].size())#seq_len,B,chans,H,W
-
-
-		return next_hidden, current_input
+		out = torch.cat(out,0).view(current_input.size(0), *out[0].size())
+		return out,next_hidden
 
 	def init_hidden(self,batch_size):
 		init_states=[]#this is a list of tuples
--- train.py
+++ train.py
@@ -2,11 +2,11 @@
 import torchvision
 import torch
 from torch import autograd
-from torch.nn.Module import Variable
+from torch.autograd import Variable
 import torch.nn.functional as F
 import bouncing_balls as b
-from Custom_CLSTM_cell import CLSTM, weights_init
-
+from conv_lstm import CLSTM, weights_init
+import numpy as np
 
 
 def generate_bouncing_ball_sample(batch_size, seq_length, shape, num_balls):
@@ -23,13 +23,17 @@ filter_size=3
 batch_size=4
 shape=(32,32)#H,W
 inp_chans=3
-nlayers=2
+nlayers=1
 seq_len=10
 
 #If using this format, then we need to transpose in CLSTM
 # input = Variable(torch.rand(batch_size,seq_len,inp_chans,shape[0],shape[1])).cuda()
 
-input = Variable(generate_bouncing_ball_sample(batch_size,seq_len, 32, 2)).cuda()
+
+dat = generate_bouncing_ball_sample(batch_size,seq_len, 32, 2)
+dat = torch.from_numpy(dat).transpose(4,3).transpose(3,2).float()
+
+input = Variable(dat).cuda()
 
 conv_lstm=CLSTM(shape, inp_chans, filter_size, num_features,nlayers)
 conv_lstm.apply(weights_init)
@@ -40,20 +44,20 @@ print 'convlstm module:',conv_lstm
 
 print 'params:'
 params=conv_lstm.parameters()
-for p in params:
-   print 'param ',p.size()
-   print 'mean ',torch.mean(p)
+#for p in params:
+#   print 'param ',p.size()
+#   print 'mean ',torch.mean(p)
 
 
 hidden_state=conv_lstm.init_hidden(batch_size)
 print 'hidden_h shape ',len(hidden_state)
 print 'hidden_h shape ',hidden_state[0][0].size()
 out=conv_lstm(input,hidden_state)
-print 'out shape',out[1].size()
-print 'len hidden ', len(out[0])
-print 'next hidden',out[0][0][0].size()
+print 'out shape',out[0].size()
+print 'len hidden ', len(out[1])
+print 'next hidden',out[1][0][0].size()
 print 'convlstm dict',conv_lstm.state_dict().keys()
 
 
-L=torch.sum(out[1])
+L=torch.sum(out[0])
 L.backward()
